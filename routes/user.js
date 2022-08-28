d79c23f2d4622621399be5788e1101800517b9d3
punarjeewarashmitha20011016 <punarjeewarashmitha1@gmail.com>
2022-08-28 23:07:29 +0530

    customer and user routes done

--- routes/user.js
+++ routes/user.js
@@ -17,3 +17,69 @@ connection.connect(function (err) {
         })
     }
 })
+
+router.get('/', (req, res) => {
+    var query = "SELECT * FROM users"
+    connection.query(query, (err, rows) => {
+        if (err) throw err
+        res.send(rows)
+    })
+})
+
+router.post('/', (req, res) => {
+    const id = req.body.id
+    const name = req.body.name
+    const username = req.body.username
+
+    var query = "INSERT INTO users (id, name, username) VALUES (?,?,?)"
+
+    connection.query(query, [id, name, username], (err) => {
+        if (err) {
+            res.send({"message": "Duplicate entry For User"})
+        } else {
+            res.send({"message": "User created!"})
+        }
+    })
+})
+
+router.put('/', (req, res) => {
+    const id = req.body.id
+    const name = req.body.name
+    const username = req.body.username
+    var query = "UPDATE users SET name =?, username=? WHERE id=?"
+    connection.query(query, [name, username, id], (err, rows) => {
+        if (err) console.log(err);
+        if (rows.affectedRows > 0) {
+            res.send({'message': 'User Updated'})
+        } else {
+            res.send({'message': 'User not found'})
+        }
+
+    })
+})
+
+router.delete('/:id', (req, res) => {
+    const id = req.params.id
+
+    var query = "DELETE FROM users WHERE id=?";
+
+    connection.query(query, [id], (err, rows) => {
+        if (err) console.log(err);
+        if (rows.affectedRows > 0) {
+            res.send({'message': 'User deleted'})
+        } else {
+            res.send({'message': 'User not found'})
+        }
+    })
+})
+
+router.get('/:id', (req, res) => {
+    const id = req.params.id
+    var query = "SELECT * FROM users WHERE id=?"
+    connection.query(query, [id], (err, rows) => {
+        if (err) console.log(err);
+        res.send(rows)
+    })
+})
+
+module.exports = router;
